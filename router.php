d9335d11faa7df6d26db167f1e6774edbdfcfd1a
ZAIN15095 <33008039+ZAIN15095@users.noreply.github.com>
2017-11-15 11:49:59 +0100

    Update router.php

--- router.php
+++ router.php
@@ -4,5 +4,12 @@ if (session_status() == PHP_SESSION_NONE)
     session_start();
 }
 
-include 'controller/controller.php';
+if (!empty($_GET["name"]))
+{
+    include 'controller/controller_'.$_GET["name"].'.php';
+}
+else
+{
+  include 'controller/controller.php';
+}
 ?>
