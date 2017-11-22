ad99426a61e75ab7ca77768fddce3a3daba2dd13
ZAIN15095 <33008039+ZAIN15095@users.noreply.github.com>
2017-11-22 11:25:06 +0100

    Update controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -16,19 +16,6 @@ else
     $reservation = new Data_reservation();
 }
 
-
-/* if(isset($_POST['destination']) && !empty($_POST['destination']))
-{
-	/$reservation->set_destination($_POST['destination']);
-	include 'view/firstpage.php';
-}
-
-if(isset($_POST['number_places']) && !empty($_POST['number_places']))
-{
-	//$reservation-> set_num_places($_POST["number_places"]);
-	include 'view/firstpage.php';
-} */
-
 if(empty($reservation->get_destination()) && empty($reservation->get_num_palces()) && empty($_POST["détails"])){
   include("view/firstpage.php");
   
